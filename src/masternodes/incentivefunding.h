DeFiCh/ain

Add Community development fund balance (#569)
* Add Community development fund balance
* Leave ability to UTXO to be converted to token via external script
* Do not mark as burnt foundation coins

Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

src/masternodes/incentivefunding.h
@@ -11,7 +11,7 @@
 #include <amount.h>
 
 inline CommunityAccountType CommunityAccountCodeToType (unsigned char ch) {
-    char const types[] = "IASFOU";
+    char const types[] = "IACSFOU";
     if (memchr(types, ch, strlen(types)))
         return static_cast<CommunityAccountType>(ch);
     else
@@ -24,6 +24,7 @@ inline char const * GetCommunityAccountName(CommunityAccountType t)
     {
         case CommunityAccountType::IncentiveFunding: return "IncentiveFunding";
         case CommunityAccountType::AnchorReward:     return "AnchorReward";
+        case CommunityAccountType::CommunityDevFunds:return "CommunityDevelopmentFunds";
         case CommunityAccountType::Swap:             return "Swap";
         case CommunityAccountType::Futures:          return "Futures";
         case CommunityAccountType::Options:          return "Options";

src/miner.cpp
@@ -236,17 +236,19 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
 
     if (nHeight >= consensus.EunosHeight)
     {
-        coinbaseTx.vout.resize(2);
+        auto foundationValue = CalculateCoinbaseReward(blockReward, consensus.dist.community);
+        if (nHeight < consensus.FortCanningHeight) {
+            coinbaseTx.vout.resize(2);
+            // Community payment always expected
+            coinbaseTx.vout[1].scriptPubKey = consensus.foundationShareScript;
+            coinbaseTx.vout[1].nValue = foundationValue;
+        }
 
         // Explicitly set miner reward
         coinbaseTx.vout[0].nValue = CalculateCoinbaseReward(blockReward, consensus.dist.masternode);
 
-        // Community payment always expected
-        coinbaseTx.vout[1].scriptPubKey = consensus.foundationShareScript;
-        coinbaseTx.vout[1].nValue = CalculateCoinbaseReward(blockReward, consensus.dist.community);
-
         LogPrint(BCLog::STAKING, "%s: post Eunos logic. Block reward %d Miner share %d foundation share %d\n",
-                 __func__, blockReward, coinbaseTx.vout[0].nValue, coinbaseTx.vout[1].nValue);
+                 __func__, blockReward, coinbaseTx.vout[0].nValue, foundationValue);
     }
     else if (nHeight >= consensus.AMKHeight) {
         // assume community non-utxo funding:

src/rpc/blockchain.cpp
@@ -162,12 +162,20 @@ UniValue blockToJSON(const CBlock& block, const CBlockIndex* tip, const CBlockIn
             CAmount burnt{0};
             for (const auto& kv : Params().GetConsensus().newNonUTXOSubsidies)
             {
+                if (blockindex->nHeight < Params().GetConsensus().FortCanningHeight
+                && kv.first == CommunityAccountType::CommunityDevFunds) {
+                    continue;
+                }
+
                 CAmount subsidy = CalculateCoinbaseReward(blockReward, kv.second);
 
-                if (kv.first == CommunityAccountType::AnchorReward) {
-                    nonutxo.pushKV(GetCommunityAccountName(kv.first), ValueFromAmount(subsidy));
-                } else {
-                    burnt += subsidy; // Everything else goes into burnt
+                switch(kv.first) {
+                    case CommunityAccountType::AnchorReward:
+                    case CommunityAccountType::CommunityDevFunds:
+                        nonutxo.pushKV(GetCommunityAccountName(kv.first), ValueFromAmount(subsidy));
+                    break;
+                    default:
+                        burnt += subsidy; // Everything else goes into burnt
                 }
             }
 

test/functional/feature_community_development_funds.py
@@ -1,0 +1,63 @@
+#!/usr/bin/env python3
+# Copyright (c) 2014-2019 The Bitcoin Core developers
+# Copyright (c) DeFi Blockchain Developers
+# Distributed under the MIT software license, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+"""Test CommunityDevelopmentFunds behaviour"""
+
+from test_framework.test_framework import DefiTestFramework
+from test_framework.util import (
+    assert_equal,
+    connect_nodes_bi
+)
+
+from decimal import Decimal
+
+class CommunityDevelopmentFunds(DefiTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 3
+        self.setup_clean_chain = True
+        self.extra_args = [
+            ['-dummypos=0', '-txnotokens=0', '-amkheight=50', '-eunosheight=80', '-fortcanningheight=201', '-subsidytest=1'],
+            ['-dummypos=0', '-txnotokens=0', '-amkheight=50', '-eunosheight=80', '-fortcanningheight=201', '-subsidytest=1'],
+            ['-dummypos=0', '-txnotokens=0', '-amkheight=50', '-eunosheight=80', '-fortcanningheight=201', '-subsidytest=1'],
+        ]
+
+    def run_test(self):
+        node0 = self.nodes[0]
+        node1 = self.nodes[1]
+        node2 = self.nodes[2]
+
+        node0.generate(200)
+        self.sync_all()
+        self.stop_node(2)
+
+        assert_equal(node0.listcommunitybalances()['CommunityDevelopmentFunds'], Decimal('0'))
+        node1.importprivkey('cMv1JaaZ9Mbb3M3oNmcFvko8p7EcHJ8XD7RCQjzNaMs7BWRVZTyR')
+        foundation = node1.getbalances()
+        foundationBalance = foundation['mine']['trusted']
+        before_hardfork = foundationBalance + foundation['mine']['immature']
+        node1.utxostoaccount({'2NCWAKfEehP3qibkLKYQjXaWMK23k4EDMVS': str(foundationBalance - Decimal("0.0010000")) + "@0"})
+        node1.generate(1)
+        self.sync_all(self.nodes[:2])
+        foundationBalance = foundation['mine']['trusted']
+        after_hardfork = foundationBalance + foundation['mine']['immature']
+        # no change
+        assert_equal(before_hardfork, after_hardfork)
+        # foundation coins are locked
+        assert_equal(node0.listcommunitybalances()['CommunityDevelopmentFunds'], foundationBalance + Decimal("19.887464") - Decimal("0.0010000"))
+        node0.generate(1)
+        self.sync_all(self.nodes[:2])
+
+        print ("Reverting...")
+        self.start_node(2)
+        node2.generate(3)
+
+        connect_nodes_bi(self.nodes, 1, 2)
+        self.sync_blocks()
+
+        assert_equal(node1.getaccount('2NCWAKfEehP3qibkLKYQjXaWMK23k4EDMVS'), [])
+        assert_equal(node0.listcommunitybalances()['CommunityDevelopmentFunds'], Decimal('{:.8f}'.format(3 * 19.887464)))
+
+if __name__ == '__main__':
+    CommunityDevelopmentFunds().main ()

solros/defichain-node

src/masternodes/incentivefunding.h
@@ -11,7 +11,7 @@
 #include <amount.h>
 
 inline CommunityAccountType CommunityAccountCodeToType (unsigned char ch) {
-    char const types[] = "IALOU";
+    char const types[] = "IACLOU";
     constexpr auto size = sizeof(types) - 1;
     if (memchr(types, ch, size))
         return static_cast<CommunityAccountType>(ch);
@@ -25,6 +25,7 @@ inline char const * GetCommunityAccountName(CommunityAccountType t)
     {
         case CommunityAccountType::IncentiveFunding: return "IncentiveFunding";
         case CommunityAccountType::AnchorReward:     return "AnchorReward";
+        case CommunityAccountType::CommunityDevFunds:return "CommunityDevelopmentFunds";
         case CommunityAccountType::Loan:             return "Loan";
         case CommunityAccountType::Options:          return "Options";
         case CommunityAccountType::Unallocated:      return "Burnt";

src/miner.cpp
@@ -233,7 +233,13 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
 
     if (nHeight >= consensus.EunosHeight)
     {
-        coinbaseTx.vout.resize(2);
+        auto foundationValue = CalculateCoinbaseReward(blockReward, consensus.dist.community);
+        if (nHeight < consensus.GreatWorldHeight) {
+            coinbaseTx.vout.resize(2);
+            // Community payment always expected
+            coinbaseTx.vout[1].scriptPubKey = consensus.foundationShareScript;
+            coinbaseTx.vout[1].nValue = foundationValue;
+        }
 
         // Explicitly set miner reward
         if (nHeight >= consensus.FortCanningHeight) {
@@ -242,12 +248,8 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
             coinbaseTx.vout[0].nValue = CalculateCoinbaseReward(blockReward, consensus.dist.masternode);
         }
 
-        // Community payment always expected
-        coinbaseTx.vout[1].scriptPubKey = consensus.foundationShareScript;
-        coinbaseTx.vout[1].nValue = CalculateCoinbaseReward(blockReward, consensus.dist.community);
-
         LogPrint(BCLog::STAKING, "%s: post Eunos logic. Block reward %d Miner share %d foundation share %d\n",
-                 __func__, blockReward, coinbaseTx.vout[0].nValue, coinbaseTx.vout[1].nValue);
+                 __func__, blockReward, coinbaseTx.vout[0].nValue, foundationValue);
     }
     else if (nHeight >= consensus.AMKHeight) {
         // assume community non-utxo funding:

src/rpc/blockchain.cpp
@@ -159,12 +159,20 @@ UniValue blockToJSON(const CBlock& block, const CBlockIndex* tip, const CBlockIn
             CAmount burnt{0};
             for (const auto& kv : Params().GetConsensus().newNonUTXOSubsidies)
             {
+                if (blockindex->nHeight < Params().GetConsensus().GreatWorldHeight
+                && kv.first == CommunityAccountType::CommunityDevFunds) {
+                    continue;
+                }
+
                 CAmount subsidy = CalculateCoinbaseReward(blockReward, kv.second);
 
-                if (kv.first == CommunityAccountType::AnchorReward) {
-                    nonutxo.pushKV(GetCommunityAccountName(kv.first), ValueFromAmount(subsidy));
-                } else {
-                    burnt += subsidy; // Everything else goes into burnt
+                switch(kv.first) {
+                    case CommunityAccountType::AnchorReward:
+                    case CommunityAccountType::CommunityDevFunds:
+                        nonutxo.pushKV(GetCommunityAccountName(kv.first), ValueFromAmount(subsidy));
+                    break;
+                    default:
+                        burnt += subsidy; // Everything else goes into burnt
                 }
             }
 

test/functional/feature_community_development_funds.py
@@ -1,0 +1,63 @@
+#!/usr/bin/env python3
+# Copyright (c) 2014-2019 The Bitcoin Core developers
+# Copyright (c) DeFi Blockchain Developers
+# Distributed under the MIT software license, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+"""Test CommunityDevelopmentFunds behaviour"""
+
+from test_framework.test_framework import DefiTestFramework
+from test_framework.util import (
+    assert_equal,
+    connect_nodes_bi
+)
+
+from decimal import Decimal
+
+class CommunityDevelopmentFunds(DefiTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 3
+        self.setup_clean_chain = True
+        self.extra_args = [
+            ['-dummypos=0', '-txnotokens=0', '-amkheight=50', '-eunosheight=80', '-greatworldheight=201', '-subsidytest=1'],
+            ['-dummypos=0', '-txnotokens=0', '-amkheight=50', '-eunosheight=80', '-greatworldheight=201', '-subsidytest=1'],
+            ['-dummypos=0', '-txnotokens=0', '-amkheight=50', '-eunosheight=80', '-greatworldheight=201', '-subsidytest=1'],
+        ]
+
+    def run_test(self):
+        node0 = self.nodes[0]
+        node1 = self.nodes[1]
+        node2 = self.nodes[2]
+
+        node0.generate(200)
+        self.sync_all()
+        self.stop_node(2)
+
+        assert_equal(node0.listcommunitybalances()['CommunityDevelopmentFunds'], Decimal('0'))
+        node1.importprivkey('cMv1JaaZ9Mbb3M3oNmcFvko8p7EcHJ8XD7RCQjzNaMs7BWRVZTyR')
+        foundation = node1.getbalances()
+        foundationBalance = foundation['mine']['trusted']
+        before_hardfork = foundationBalance + foundation['mine']['immature']
+        node1.utxostoaccount({'2NCWAKfEehP3qibkLKYQjXaWMK23k4EDMVS': str(foundationBalance - Decimal("0.0010000")) + "@0"})
+        node1.generate(1)
+        self.sync_all(self.nodes[:2])
+        foundationBalance = foundation['mine']['trusted']
+        after_hardfork = foundationBalance + foundation['mine']['immature']
+        # no change
+        assert_equal(before_hardfork, after_hardfork)
+        # foundation coins are locked
+        assert_equal(node0.listcommunitybalances()['CommunityDevelopmentFunds'], foundationBalance + Decimal("19.887464") - Decimal("0.0010000"))
+        node0.generate(1)
+        self.sync_all(self.nodes[:2])
+
+        print ("Reverting...")
+        self.start_node(2)
+        node2.generate(3)
+
+        connect_nodes_bi(self.nodes, 1, 2)
+        self.sync_blocks()
+
+        assert_equal(node1.getaccount('2NCWAKfEehP3qibkLKYQjXaWMK23k4EDMVS'), [])
+        assert_equal(node0.listcommunitybalances()['CommunityDevelopmentFunds'], Decimal('{:.8f}'.format(3 * 19.887464)))
+
+if __name__ == '__main__':
+    CommunityDevelopmentFunds().main ()